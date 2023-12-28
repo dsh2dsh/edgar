73ee6803d5f52bdba77be0fd89b12f9f47f57a07
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2023-12-28 21:41:47 +0100

    Add "frame" into fact_units

--- db/schema.sql
+++ db/schema.sql
@@ -44,5 +44,6 @@ CREATE TABLE fact_units (
   fp          TEXT    NOT NULL,
   form        TEXT    NOT NULL,
   filed       DATE    NOT NULL,
+  frame       TEXT,
   PRIMARY KEY (company_cik, fact_id, unit_id)
 );
