de59f1de43ba420c9bb93881eb081ae39eae7e00
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2023-12-30 21:18:56 +0100

    edgar db upload is functional now

--- cmd/db.go
+++ cmd/db.go
@@ -2,10 +2,13 @@ package cmd
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
+	"net/http"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/caarlos0/env/v10"
 	"github.com/cespare/xxhash/v2"
@@ -16,6 +19,7 @@ import (
 	"golang.org/x/sync/singleflight"
 
 	"github.com/dsh2dsh/edgar/client"
+	"github.com/dsh2dsh/edgar/cmd/internal/common"
 	"github.com/dsh2dsh/edgar/internal/repo"
 )
 
@@ -65,7 +69,7 @@ and initialize it:
 			defer db.Close()
 			cobra.CheckErr(db.Ping(ctx))
 
-			edgar, err := newClient()
+			edgar, err := common.NewClient()
 			cobra.CheckErr(err)
 
 			uploader := NewUpload(edgar, repo.New(db)).WithProcsLimit(uploadProcs)
@@ -117,7 +121,7 @@ func connString() (string, error) {
 	return cfg.ConnURL, nil
 }
 
-func NewUpload(edgar *client.Client, repo *repo.Repo) *Upload {
+func NewUpload(edgar *client.Client, repo Repo) *Upload {
 	return &Upload{
 		edgar: edgar,
 		repo:  repo,
@@ -129,9 +133,20 @@ func NewUpload(edgar *client.Client, repo *repo.Repo) *Upload {
 	}
 }
 
+type Repo interface {
+	AddCompany(ctx context.Context, cik uint32, name string) (bool, error)
+	AddFact(ctx context.Context, tax, name string) (uint32, error)
+	AddLabel(ctx context.Context, factId uint32, label, descr string,
+		labelHash, descrHash uint64) error
+	AddUnit(ctx context.Context, name string) (uint32, error)
+	AddFactUnit(ctx context.Context, fact repo.FactUnit) error
+	CopyFactUnits(ctx context.Context, len int,
+		next func(i int) (repo.FactUnit, error)) error
+}
+
 type Upload struct {
 	edgar *client.Client
-	repo  *repo.Repo
+	repo  Repo
 
 	knownFacts facts
 	knownUnits factUnits
@@ -148,52 +163,59 @@ func (self *Upload) Upload() error {
 	g, ctx := errgroup.WithContext(context.Background())
 	g.SetLimit(self.procs)
 
-	log.Println("requesting list of companies...")
+	log.Println("fetch companies")
 	companies, err := self.edgar.CompanyTickers(ctx)
 	if err != nil {
-		return fmt.Errorf("list of companies: %w", err)
+		return fmt.Errorf("fetch companies: %w", err)
 	}
-	log.Printf("got %v companies", len(companies))
+	log.Printf("%v companies", len(companies))
 
 	for i, company := range companies {
+		if i == 2 { // TODO: remove
+			break
+		}
 		if ctx.Err() != nil {
 			break
 		}
 		cik := company.CIK
-		log.Printf("processing company %v/%v CIK=%v: %v (%v)...", i, len(companies), cik,
+		log.Printf("%v/%v: company CIK=%v %q: %q", i+1, len(companies), cik,
 			company.Ticker, company.Title)
-		g.Go(func() error { return self.processCompany(ctx, cik) })
+		g.Go(func() error { return self.processCompanyFacts(ctx, cik) })
 	}
 
 	if err := g.Wait(); err != nil {
-		return fmt.Errorf("upload edgar facts: %w", err)
+		return fmt.Errorf("upload facts: %w", err)
 	}
 	return nil
 }
 
-func (self *Upload) processCompany(ctx context.Context, cik uint32) error {
-	company, err := self.edgar.CompanyFacts(ctx, cik)
+func (self *Upload) processCompanyFacts(ctx context.Context, cik uint32) error {
+	company, err := self.companyFacts(ctx, cik)
 	if err != nil {
-		return fmt.Errorf("facts of CIK=%v: %w", cik, err)
-	}
-	log.Printf("got facts of CIK=%v: %q", company.CIK, company.EntityName)
-
-	isNewCompany, err := self.repo.AddCompany(ctx, company.CIK, company.EntityName)
-	if err != nil {
-		return fmt.Errorf("upload: %w", err)
-	} else if isNewCompany {
-		log.Printf("new company added: CIK=%v %q", company.CIK, company.EntityName)
+		var status *client.UnexpectedStatusError
+		if errors.As(err, &status) && status.StatusCode() == http.StatusNotFound {
+			log.Printf("skip company: %s", err)
+			return nil
+		}
+		return err
 	}
 
 	for taxName, facts := range company.Facts {
 		for factName, fact := range facts {
-			_, err := self.addFact(ctx, taxName, factName, fact.Label,
+			factId, err := self.addFact(ctx, taxName, factName, fact.Label,
 				fact.Description)
 			if err != nil {
-				return fmt.Errorf("add fact for company CIK=%v: %w", company.CIK, err)
+				return fmt.Errorf("processCompanyFacts: company CIK=%v: %w",
+					company.CIK, err)
 			}
-			for unitName := range fact.Units {
-				_, err := self.addUnit(ctx, unitName)
+			for unitName, factUnits := range fact.Units {
+				unitId, err := self.addUnit(ctx, unitName)
+				if err != nil {
+					return err
+				}
+				log.Printf(`%v facts of CIK=%v, "%v:%v", %q`, len(factUnits),
+					company.CIK, taxName, factName, unitName)
+				err = self.addFactUnits(ctx, company.Id(), factId, unitId, factUnits)
 				if err != nil {
 					return err
 				}
@@ -204,49 +226,120 @@ func (self *Upload) processCompany(ctx context.Context, cik uint32) error {
 	return nil
 }
 
+func (self *Upload) companyFacts(ctx context.Context, cik uint32,
+) (client.CompanyFacts, error) {
+	facts, err := self.edgar.CompanyFacts(ctx, cik)
+	if err != nil {
+		return facts, fmt.Errorf("facts of CIK=%v: %w", cik, err)
+	}
+	log.Printf("got facts: CIK=%v: %q", facts.CIK, facts.EntityName)
+
+	unknownCompany, err := self.repo.AddCompany(ctx, facts.Id(), facts.EntityName)
+	if err != nil {
+		return facts, fmt.Errorf("companyFacts: %w", err)
+	} else if unknownCompany {
+		log.Printf("add company: CIK=%v %q", facts.CIK, facts.EntityName)
+	}
+
+	return facts, nil
+}
+
 func (self *Upload) addFact(ctx context.Context, tax, name, label, descr string,
 ) (uint32, error) {
 	factKey := strings.Join([]string{tax, name}, ":")
 	labelHash := xxhash.Sum64String(label)
 	descrHash := xxhash.Sum64String(descr)
 
-	fact, ok := self.knownFacts.Fact(factKey)
-	if !ok {
-		newFact, err := self.knownFacts.Create(factKey, labelHash, descrHash,
-			func() (uint32, error) {
-				log.Printf("creating new fact %q...", factKey)
-				//nolint:wrapcheck // will wrap below
-				return self.repo.AddFact(ctx, tax, name)
-			})
+	addLabel := func(factId uint32) error {
+		log.Printf("add label fact %q: label=%#x, descr=%#x", factKey, labelHash,
+			descrHash)
+		err := self.repo.AddLabel(ctx, factId, label, descr, labelHash, descrHash)
 		if err != nil {
-			return 0, fmt.Errorf("failed create fact %q: %w", factKey, err)
+			return fmt.Errorf("failed add label fact %q: %w", factKey, err)
 		}
-		fact = newFact
+		return nil
 	}
 
-	err := fact.AddLabel(ctx, labelHash, descrHash, func() error {
-		log.Printf("creating new label for fact %q: %#x, %#x...",
-			factKey, labelHash, descrHash)
-		//nolint:wrapcheck // will wrap below
-		return self.repo.AddLabel(ctx, fact.Id, label, descr, labelHash, descrHash)
-	})
+	if fact, ok := self.knownFacts.Fact(factKey); ok {
+		return fact.Id, fact.AddLabel(ctx, labelHash, descrHash, func() error {
+			return addLabel(fact.Id)
+		})
+	}
+
+	fact, err := self.knownFacts.Create(factKey, labelHash, descrHash,
+		func() (uint32, error) {
+			log.Printf("add fact %q: label=%#x, descr=%#x", factKey, labelHash,
+				descrHash)
+			factId, err := self.repo.AddFact(ctx, tax, name)
+			if err != nil {
+				return 0, err //nolint:wrapcheck // will wrap below
+			}
+			return factId, addLabel(factId)
+		})
 	if err != nil {
-		return 0, fmt.Errorf("failed add label for fact %q: %w", factKey, err)
+		return 0, fmt.Errorf("failed add fact %q: %w", factKey, err)
 	}
-
 	return fact.Id, nil
 }
 
 func (self *Upload) addUnit(ctx context.Context, name string) (uint32, error) {
 	unitId, err := self.knownUnits.Id(ctx, name, func() (uint32, error) {
-		log.Printf("creating new unit %q...", name)
+		log.Printf("add unit %q", name)
 		//nolint:wrapcheck // will wrap below
 		return self.repo.AddUnit(ctx, name)
 	})
 	if err != nil {
-		return unitId, fmt.Errorf("failed create unit %q: %w", name, err)
+		return unitId, fmt.Errorf("failed add unit %q: %w", name, err)
 	}
 	return unitId, nil
+}
+
+func (self *Upload) addFactUnits(ctx context.Context, cik uint32,
+	factId, unitId uint32, clientFacts []client.FactUnit,
+) error {
+	err := self.repo.CopyFactUnits(ctx, len(clientFacts),
+		func(i int) (repo.FactUnit, error) {
+			return self.repoFactUnit(cik, factId, unitId, &clientFacts[i])
+		})
+	if err != nil {
+		return fmt.Errorf("failed add %v facts: cik=%v, factId=%v, unitId=%v: %w",
+			len(clientFacts), cik, factId, unitId, err)
+	}
+	return nil
+}
+
+func (self *Upload) repoFactUnit(cik uint32, factId, unitId uint32,
+	clientFact *client.FactUnit,
+) (repo.FactUnit, error) {
+	fact := repo.FactUnit{
+		CIK:    cik,
+		FactId: factId,
+		UnitId: unitId,
+		End:    time.Time{},
+		Val:    clientFact.Val,
+		Accn:   clientFact.Accn,
+		FY:     clientFact.FY,
+		FP:     clientFact.FP,
+		Form:   clientFact.Form,
+		Filed:  time.Time{},
+	}
+
+	if clientFact.Frame != "" {
+		fact.WithFrame(clientFact.Frame)
+	}
+
+	err := clientFact.ParseTimes(func(startTime, endTime, filedTime time.Time) {
+		if !startTime.IsZero() {
+			fact.WithStart(startTime)
+		}
+		fact.End = endTime
+		fact.Filed = filedTime
+	})
+	if err != nil {
+		return fact, fmt.Errorf("convert FactUnit from client to repo: %w", err)
+	}
+
+	return fact, nil
 }
 
 // --------------------------------------------------
--- cmd/db_test.go
+++ cmd/db_test.go
@@ -4,10 +4,230 @@ import (
 	"context"
 	"errors"
 	"testing"
+	"time"
 
+	"github.com/dsh2dsh/edgar/client"
+	"github.com/dsh2dsh/edgar/internal/repo"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+func TestUpload_repoFactUnit(t *testing.T) {
+	const appleCIK = 320193
+	const factId = 1
+	const unitId = 2
+
+	clientFact := client.FactUnit{
+		Start: "2008-09-27",
+		End:   "2008-09-27",
+		Val:   5520000000,
+		Accn:  "0001193125-09-153165",
+		FY:    2009,
+		FP:    "Q3",
+		Form:  "10-Q",
+		Filed: "2009-07-22",
+		Frame: "CY2008Q3I",
+	}
+
+	repoFact := repo.FactUnit{
+		CIK:    appleCIK,
+		FactId: factId,
+		UnitId: unitId,
+		End:    time.Date(2008, 9, 27, 0, 0, 0, 0, time.UTC),
+		Val:    5520000000,
+		Accn:   "0001193125-09-153165",
+		FY:     2009,
+		FP:     "Q3",
+		Form:   "10-Q",
+		Filed:  time.Date(2009, 7, 22, 0, 0, 0, 0, time.UTC),
+	}
+
+	tests := []struct {
+		name     string
+		prepare  func(t *testing.T, fact *client.FactUnit)
+		repoFact func() repo.FactUnit
+		wantErr  bool
+	}{
+		{
+			name: "ok",
+			repoFact: func() repo.FactUnit {
+				fact := repoFact
+				fact.WithStart(time.Date(2008, 9, 27, 0, 0, 0, 0, time.UTC)).
+					WithFrame("CY2008Q3I")
+				return fact
+			},
+		},
+		{
+			name: "without Start",
+			prepare: func(t *testing.T, fact *client.FactUnit) {
+				fact.Start = ""
+			},
+			repoFact: func() repo.FactUnit {
+				fact := repoFact
+				fact.WithFrame("CY2008Q3I")
+				return fact
+			},
+		},
+		{
+			name: "without Frame",
+			prepare: func(t *testing.T, fact *client.FactUnit) {
+				fact.Frame = ""
+			},
+			repoFact: func() repo.FactUnit {
+				fact := repoFact
+				fact.WithStart(time.Date(2008, 9, 27, 0, 0, 0, 0, time.UTC))
+				return fact
+			},
+		},
+		{
+			name: "invalid date",
+			prepare: func(t *testing.T, fact *client.FactUnit) {
+				fact.Filed = "not a date"
+			},
+			repoFact: func() repo.FactUnit {
+				fact := repoFact
+				fact.WithFrame("CY2008Q3I")
+				return fact
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fact := clientFact
+			if tt.prepare != nil {
+				tt.prepare(t, &fact)
+			}
+			u := Upload{}
+			gotFact, err := u.repoFactUnit(appleCIK, factId, unitId, &fact)
+			if tt.wantErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				assert.Equal(t, tt.repoFact(), gotFact)
+			}
+		})
+	}
+}
+
+func TestFacts_Fact(t *testing.T) {
+	const factKey = "us-gaap:AccountsPayable"
+
+	tests := []struct {
+		name       string
+		assertCall func(t *testing.T, facts *facts) (*knownFact, error)
+		errorIs    error
+		wantFact   *knownFact
+	}{
+		{
+			name: "Fact hit",
+			assertCall: func(t *testing.T, facts *facts) (*knownFact, error) {
+				facts.knownFacts[factKey] = newKnownFact(0, 1, 1)
+				fact, ok := facts.Fact(factKey)
+				assert.True(t, ok)
+				return fact, nil
+			},
+			wantFact: newKnownFact(0, 1, 1),
+		},
+		{
+			name: "Fact miss",
+			assertCall: func(t *testing.T, facts *facts) (*knownFact, error) {
+				fact, ok := facts.Fact(factKey)
+				assert.False(t, ok)
+				return fact, nil
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			facts := newFacts()
+			fact, err := tt.assertCall(t, &facts)
+			if tt.errorIs != nil {
+				require.ErrorIs(t, err, tt.errorIs)
+			} else {
+				require.NoError(t, err)
+			}
+			assert.Equal(t, tt.wantFact, fact)
+		})
+	}
+}
+
+func TestFacts_Create(t *testing.T) {
+	const factKey = "us-gaap:AccountsPayable"
+
+	callbackCalled := func(t *testing.T, factId uint32, err error) func() (uint32, error) {
+		var called bool
+		t.Cleanup(func() { assert.True(t, called, "callback wasn't called") })
+		return func() (uint32, error) {
+			called = true
+			return factId, err
+		}
+	}
+
+	callbackNotCalled := func(t *testing.T) func() (uint32, error) {
+		return func() (uint32, error) {
+			assert.Fail(t, "Shouldn't be called")
+			return 0, nil
+		}
+	}
+
+	wantErr := errors.New("test error")
+
+	tests := []struct {
+		name       string
+		assertCall func(t *testing.T, facts *facts) (*knownFact, error)
+		errorIs    error
+		wantFact   *knownFact
+		wantFacts  map[string]*knownFact
+	}{
+		{
+			name: "hit inside group",
+			assertCall: func(t *testing.T, facts *facts) (*knownFact, error) {
+				facts.knownFacts[factKey] = newKnownFact(1, 2, 2)
+				fact, err := facts.Create(factKey, 2, 2, callbackNotCalled(t))
+				return fact, err
+			},
+			wantFact:  newKnownFact(1, 2, 2),
+			wantFacts: map[string]*knownFact{factKey: newKnownFact(1, 2, 2)},
+		},
+		{
+			name: "with callback",
+			assertCall: func(t *testing.T, facts *facts) (*knownFact, error) {
+				fact, err := facts.Create(factKey, 2, 2, callbackCalled(t, 1, nil))
+				return fact, err
+			},
+			wantFact:  newKnownFact(1, 2, 2),
+			wantFacts: map[string]*knownFact{factKey: newKnownFact(1, 2, 2)},
+		},
+		{
+			name: "with callback error",
+			assertCall: func(t *testing.T, facts *facts) (*knownFact, error) {
+				fact, err := facts.Create(factKey, 2, 2, callbackCalled(t, 0, wantErr))
+				return fact, err
+			},
+			errorIs:   wantErr,
+			wantFacts: map[string]*knownFact{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			facts := newFacts()
+			fact, err := tt.assertCall(t, &facts)
+			if tt.errorIs != nil {
+				require.ErrorIs(t, err, tt.errorIs)
+				require.Nil(t, fact)
+			} else {
+				require.NoError(t, err)
+				require.NotNil(t, fact)
+			}
+			assert.Equal(t, tt.wantFact, fact)
+			assert.Equal(t, tt.wantFacts, facts.knownFacts)
+		})
+	}
+}
 
 func TestKnownFact_AddLabel(t *testing.T) {
 	callbackCalled := func(t *testing.T, err error) func() error {
@@ -125,7 +345,7 @@ func TestKnownFact_AddLabel(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			fact := &knownFact{LabelHash: 1, DescrHash: 1}
+			fact := newKnownFact(0, 1, 1)
 			err := tt.assertCall(t, fact)
 			if tt.errorIs != nil {
 				require.ErrorIs(t, err, tt.errorIs)
